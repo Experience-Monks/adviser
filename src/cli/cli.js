21646e4e569c2a751c8576c83055e325f6b35ee1
Iran Reyes <iran.reyes@jam3.com>
2019-05-24 15:12:19 -0400

    chore: improve logging for debugging in the cli

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -31,10 +31,13 @@ class CLI {
     let errorCode = 0;
 
     try {
+      debug('Parsing the CLI options');
       currentOptions = options.parse(args);
     } catch (error) {
       logger.error(error.message);
       logger.info(`\nRun 'adviser -h' to discover the available parameters`);
+
+      debug('Error parsing the CLI options', error);
 
       errorCode = 2;
       callback(errorCode);
@@ -42,16 +45,16 @@ class CLI {
     }
 
     if (currentOptions.version) {
+      debug('Retrieving package version');
       logger.info(`v${packageInfo.getVersion()}`);
     } else if (currentOptions.help) {
+      debug('Retrieving CLI help');
       logger.info(options.generateHelp());
     } else {
-      debug('Adviser running the engine');
-
+      debug(`Parsing CLI arguments ${JSON.stringify(currentOptions)} for the engine`);
       const engineOptions = this._prepareEngineOptions(currentOptions);
 
-      const config = new Config(engineOptions.cwd);
-
+      const config = new Config(engineOptions['configFile'] || engineOptions.cwd);
       const engine = new Engine(config, engineOptions);
 
       engine
@@ -97,12 +100,11 @@ class CLI {
    */
   _prepareEngineOptions(cliOptions) {
     const engineOptions = {
-      outputFormat: cliOptions.format,
+      outputFormat: 'base',
       cwd: process.cwd()
     };
 
     cliOptions.config && (engineOptions['configFile'] = cliOptions.config);
-    cliOptions.output && (engineOptions['outputDirectory'] = cliOptions.output);
 
     return engineOptions;
   }
